7e4b54d0a82451de74613b58cac1373773899579
Yusha D <jojomemes@yandex.ru>
2023-02-14 00:37:46 +0400

    commit

---
+++ task_21/BinaryTree.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class BinaryTree
+    {
+        private class Node
+        {
+            public int inf; 
+            public Node left; 
+            public Node right; 
+                               
+            public Node(int nodeInf)
+            {
+                inf = nodeInf;
+                left = null;
+                right = null;
+            }
+            
+            public static void Add(ref Node r, int nodeInf)
+            {
+                if (r == null)
+                {
+                    r = new Node(nodeInf);
+                }
+                else
+                {
+                    
+                if (((IComparable)(r.inf)).CompareTo(nodeInf) > 0)
+                    {
+                        Add(ref r.left, nodeInf);
+                    }
+                    else
+                    {
+                        Add(ref r.right, nodeInf);
+                    }
+                }
+            }
+            public static void Preorder(Node r) 
+            {
+                if (r != null)
+                {
+                    Console.Write("{0} ", r.inf);
+                    Preorder(r.left);
+                    Preorder(r.right);
+                }
+            }
+            public static void Inorder(Node r) 
+            {
+                if (r != null)
+                {
+                    Inorder(r.left);
+                    Console.Write("{0} ", r.inf);
+                    Inorder(r.right);
+                }
+            }
+            public static void Postorder(Node r)
+            {
+                if (r != null)
+                {
+                    Postorder(r.left);
+                    Postorder(r.right);
+                    Console.Write("{0} ", r.inf);
+                }
+            }
+            
+            public static void Search(Node r, object key, out Node item)
+            {
+                if (r == null)
+                {
+                    item = null;
+                }
+                else
+                {
+                    if (((IComparable)(r.inf)).CompareTo(key) == 0)
+                    {
+                        item = r;
+                    }
+                    else
+                       
+                {
+                        if (((IComparable)(r.inf)).CompareTo(key) > 0)
+                        {
+                            Search(r.left, key, out item);
+                        }
+                        else
+                        {
+                            Search(r.right, key, out item);
+                        }
+                    }
+                }
+            }
+            
+            private static void Del(Node t, ref Node tr)
+            {
+                if (tr.right != null)
+                {
+                    Del(t, ref tr.right);
+                }
+                else
+                {
+                    t.inf = tr.inf;
+                    tr = tr.left;
+                }
+            }
+            public static void Delete(ref Node t, object key)
+            {
+                if (t == null)
+                {
+                    throw new Exception("Данное значение в дереве отсутствует");
+                }
+                else
+                {
+                    if (((IComparable)(t.inf)).CompareTo(key) > 0)
+                    {
+                        Delete(ref t.left, key);
+                    }
+                    else
+                    {
+                        if (((IComparable)(t.inf)).CompareTo(key) < 0)
+                        {
+                            Delete(ref t.right, key);
+                        }
+                        else
+                        {
+                            if (t.left == null)
+                            {
+                                t = t.right;
+                            }
+                            else
+                            {
+                                
+                            if (t.right == null)
+                                {
+                                    t = t.left;
+                                }
+                                else
+                                {
+                                    Del(t, ref t.left);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            public static void OnlyRight(Node r, ref int k)
+            {
+                if (r.left == null && r.right != null)
+                {
+                    k += r.inf;
+                }
+                if (r.left != null)
+                    OnlyRight(r.left, ref k);
+                if (r.right != null)
+                    OnlyRight(r.right, ref k);
+            }
+        } 
+        Node tree; 
+                   
+        public int Inf
+        {
+            set { tree.inf = value; }
+            get { return tree.inf; }
+        }
+        public BinaryTree()
+        {
+            tree = null;
+        }
+        private BinaryTree(Node r)
+        {
+            tree = r;
+        }
+        public void Add(int nodeInf) 
+        {
+            Node.Add(ref tree, nodeInf);
+        }
+        
+        public void Preorder()
+        {
+            Node.Preorder(tree);
+            Console.WriteLine();
+        }
+        public void Inorder()
+        {
+            Node.Inorder(tree);
+            Console.WriteLine();
+        }
+        public void Postorder()
+        {
+            Node.Postorder(tree);
+            Console.WriteLine();
+        }
+        
+        public BinaryTree Search(object key)
+        {
+            Node r;
+            Node.Search(tree, key, out r);
+            BinaryTree t = new BinaryTree(r);
+            return t;
+        }
+        //удаление ключевого узла в дереве
+        public void Delete(object key)
+        {
+            Node.Delete(ref tree, key);
+        }
+
+        public int OnlyRight() 
+        {
+            int k = 0;
+            Node.OnlyRight(tree, ref k);
+            return k;
+        }
+    }
+}
